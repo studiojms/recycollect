e0555227f8c80a3250e3ebff05ba62fa57dd5ede
jefferson.souza <jefferson.souza@synchro.com.br>
2020-06-07 10:19:53 -0300

    added backend data validation

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import multer from 'multer';
+import { celebrate, Joi } from 'celebrate';
 
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
@@ -16,7 +17,26 @@ routes.get('/items', itemsController.list);
 
 routes.get('/points', pointsController.list);
 routes.get('/points/:id', pointsController.show);
-routes.post('/points', upload.single('image'), pointsController.create);
+routes.post(
+  '/points',
+  upload.single('image'),
+  celebrate(
+    {
+      body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required(),
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+        city: Joi.string().required(),
+        state: Joi.string().required().max(2),
+        items: Joi.string().required(),
+      }),
+    },
+    { abortEarly: false }
+  ),
+  pointsController.create
+);
 
 routes.get('/', (req, res) => {
   return res.json({ message: 'Welcome to recycollect' });
